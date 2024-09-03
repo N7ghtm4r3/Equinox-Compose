c917b44c4355154866d14a3e3813b6207467bc2c
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-09-03 17:20:21 +0200

    Working on the WriteableText component

--- library/src/commonMain/kotlin/com/tecknobit/equinoxcompose/components/WriteableText.kt
+++ library/src/commonMain/kotlin/com/tecknobit/equinoxcompose/components/WriteableText.kt
@@ -1,14 +1,19 @@
 package com.tecknobit.equinoxcompose.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 private lateinit var isInEditMode: MutableState<Boolean>
@@ -25,6 +30,7 @@ fun WriteableText(
         focusedTextColor = Color.White,
         unfocusedTextColor = Color.White
     ),
+    writeableTextWidth: Dp = 280.dp,
     writeableText: MutableState<String>,
     writeableTextStyle: TextStyle = LocalTextStyle.current.merge(
         fontSize = 20.sp
@@ -46,14 +52,15 @@ fun WriteableText(
     if(isInEditMode.value) {
         EquinoxTextField(
             textFieldColors = writeableTextColors,
+            width = writeableTextWidth,
             value = writeableText,
             onValueChange = onTyping,
             placeholder = placeholder,
             placeholderStyle = placeholderStyle,
+            textFieldStyle = writeableTextStyle,
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Done
             ),
-            textFieldStyle = writeableTextStyle,
             keyboardActions = KeyboardActions(
                 onDone = { isInEditMode.value = false }
             )
@@ -68,4 +75,25 @@ fun WriteableText(
             style = displayedTextStyle
         )
     }
+}
+
+fun Modifier.disableEditModeOnTap() = this
+    .pointerInput(Unit) {
+        detectTapGestures {
+            disableEditMode()
+        }
+    }
+
+fun Modifier.disableEditModeOnFocusGain() = this
+    .onFocusEvent { event ->
+        if(event.hasFocus)
+            disableEditMode()
+    }
+
+fun enableEditMode() {
+    isInEditMode.value = true
+}
+
+fun disableEditMode() {
+    isInEditMode.value = false
 }
